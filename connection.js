dd25bdd56464d812159de092060394862abb3127
kartik singh <kart.singh15@gmail.com>
2017-06-11 18:46:45 +0530

    Password Update

--- connection.js
+++ connection.js
@@ -1,5 +1,5 @@
 var sequelize = require('sequelize'),
-    seq = new sequelize('scrapp', 'root', 'kartik95', {
+    seq = new sequelize('scrapp', 'root', 'scrappfree', {
         host: 'localhost',
         dialect: 'mysql',
         pool: {
